77f1b514cdb173f774dab82977ada2acda4a1df4
You9977 <kaounakh0205@yahoo.co.jp>
2021-10-14 21:49:47 +0900

    add attachment validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,14 @@ class Post < ApplicationRecord
 
   validates :title, length: { maximum: 32 }, presence: true
   validate  :validate_content_length
-  
+  validate  :validate_content_attachable_byte_size
+
   MAX_CONTENT_LENGTH = 50
+  ONE_KILOBYTE = 1024
+  MEGA_BYTES = 0.03
+  MAX_CONTENT_ATTACHMENT_BYTE_SIZE = MEGA_BYTES * 1000 * ONE_KILOBYTE
+
+  private
 
   def validate_content_length
     length = content.to_plain_text.length
@@ -16,6 +22,21 @@ class Post < ApplicationRecord
         max_content_length: MAX_CONTENT_LENGTH,
         length: length
       )
+    end
+  end
+
+  def validate_content_attachable_byte_size
+    byte_size = content.body.attachables.grep(ActiveStorage::Blob).each do |attachable|
+
+      if attachable.byte_size > MAX_CONTENT_ATTACHMENT_BYTE_SIZE
+        errors.add(
+          :base,
+          :content_attachment_byte_size_is_too_big,
+          max_byte_size: MEGA_BYTES,
+          byte_size: attachable.byte_size,
+          max_byte: MAX_CONTENT_ATTACHMENT_BYTE_SIZE
+        )
+      end
     end
   end
 end
